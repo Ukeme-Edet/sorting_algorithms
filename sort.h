91ad40d2f12333e810e9a6f431deae676d083c45
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-22 01:46:21 +0100

    Cocktail shaker sort

--- sort.h
+++ sort.h
@@ -18,6 +18,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
@@ -29,5 +40,8 @@ void quick_sort_helper(int *array, int lo, int hi, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
 void swap_ints(int *a, int *b);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 
 #endif /* SORT_H */
