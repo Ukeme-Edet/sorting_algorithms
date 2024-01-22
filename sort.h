8b0db893ec17b7a004a410ca1043d71638fcb429
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-22 01:42:38 +0100

    Shell sort - Knuth Sequence

--- sort.h
+++ sort.h
@@ -27,5 +27,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_helper(int *array, int lo, int hi, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
+void swap_ints(int *a, int *b);
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
